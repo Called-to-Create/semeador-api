750cdb766de00c31cc005115a46c3cb160c8f1a7
Junior Streichan <juniorstreichan.dev@gmail.com>
2021-02-05 08:34:51 -0400

    refactor: mudança na modelagem

--- src/modules/place/Place.js
+++ src/modules/place/Place.js
@@ -1,17 +1,43 @@
 import { model, Schema } from "mongoose";
 
+const AddressSchema = new Schema({
+  road: String,
+  suburb: String,
+  city: String,
+  county: String,
+  state_district: String,
+  state: String,
+  postcode: String,
+  country: String,
+  country_code: String,
+  number: Number,
+});
+
+const MailSchema = new Schema({
+  date: Date,
+  postman: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+});
+
 const PlaceSchema = new Schema(
   {
-    road: String,
-    suburb: String,
-    city: String,
-    county: String,
-    state_district: String,
-    state: String,
-    postcode: String,
-    country: String,
-    country_code: String,
-    number: Number,
+    projectId: {
+      type: String,
+      required: true,
+    },
+    createdFor: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    address: {
+      type: AddressSchema,
+      required: true,
+    },
+    envelopes: [MailSchema],
     obs: String,
     location: {
       type: {
--- src/modules/place/place.routes.js
+++ src/modules/place/place.routes.js
@@ -5,20 +5,42 @@ const placeRoutes = Router();
 
 placeRoutes.endpointBase = "/places";
 
-placeRoutes.post("/mailing", async (req, res) => {
-  try {
-    const place = req.body;
-    place.location = {
-      type: "Point",
-      coordinates: [place.location.lat, place.location.lng],
-    };
+placeRoutes.post("/mailing", (req, res) => {
+  // TODO: validar projectId
+  // TODO: duplicação de place por lat long
+  const user = req.headers["user"];
+  const projectId = req.headers["project_id"];
 
-    const newPlace = await Place.create(place);
+  const placeRequest = req.body;
+//   console.log(placeRequest);
 
-    return res.json(newPlace);
-  } catch (error) {
-    return res.statusCode(500).json(error);
-  }
+  const place = {
+    obs: placeRequest.obs,
+    projectId,
+    createdFor: user,
+    address: {
+      road: placeRequest.road,
+      suburb: placeRequest.suburb,
+      city: placeRequest.city,
+      county: placeRequest.county,
+      state_district: placeRequest.state_district,
+      state: placeRequest.state,
+      postcode: placeRequest.postcode,
+      country: placeRequest.country,
+      country_code: placeRequest.country_code,
+      number: placeRequest.number,
+    },
+    envelopes: [{ date: Date(), postman: user }],
+  };
+
+  place.location = {
+    type: "Point",
+    coordinates: [placeRequest.location.lat, placeRequest.location.lng],
+  };
+
+  Place.create(place)
+    .then((newPlace) => res.status(201).json(newPlace))
+    .catch((error) => res.status(500).json(error));
 });
 
 export default placeRoutes;
